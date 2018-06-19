80678378b52b8c6ee3a16bf2049349d043a33d11
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-06-19 14:26:10 +0200

    ListaCoches
    lista coches con su adapter y view holder

---
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/Adapters/ListaCochesAdapters.java
@@ -0,0 +1,71 @@
+package com.example.alejandro.examenjuniopmdm.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.alejandro.examenjuniopmdm.FBObjects.FBCoche;
+import com.example.alejandro.examenjuniopmdm.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alejandro on 19/6/18.
+ */
+
+  class ListaCochesAdapter extends RecyclerView.Adapter<CocheViewHolder> {
+
+    private ArrayList<FBCoche> coches;
+
+    public ListaCochesAdapter(ArrayList<FBCoche> coches){
+        this.coches=coches;
+
+    }
+
+
+    @Override
+    public CocheViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.celda_coche_layout, null);
+        CocheViewHolder cocheViewHolder = new CocheViewHolder(view);
+        return cocheViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CocheViewHolder holder, int position) {
+        // holder.textomensaje.setText(coches.get(position).original);
+        holder.tvfabricado.setText(coches.get(position).Fabricado+"");
+        holder.tvmarca.setText(coches.get(position).Marca);
+        holder.tvnombre.setText(coches.get(position).Nombre);
+        holder.tvlat.setText(coches.get(position).lat + "");
+        holder.tvlon.setText(coches.get(position).lon+"");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return coches.size();
+    }
+}
+class CocheViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView tvfabricado;
+    public TextView tvmarca;
+    public TextView tvnombre;
+    public TextView tvlat;
+    public TextView tvlon;
+
+
+
+
+    public CocheViewHolder(View itemView) {
+        super(itemView);
+        tvfabricado=itemView.findViewById(R.id.tvfabricado);
+        tvmarca=itemView.findViewById(R.id.tvmarca);
+        tvnombre=itemView.findViewById(R.id.tvnombre);
+        tvlat=itemView.findViewById(R.id.tvlat);
+        tvlon=itemView.findViewById(R.id.tvlon);
+
+    }
+}
--- app/src/main/java/com/example/alejandro/examenjuniopmdm/SecondActivity.java
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/SecondActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.alejandro.examenjuniopmdm.FBObjects.FBCoche;
 import com.example.alejandro.examenjuniopmdm.FBObjects.Mensaje;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.GenericTypeIndicator;
@@ -13,7 +14,7 @@ import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
 
-    public ListaMensajesFragment listaMensajesFragment;
+    public ListaMensajesFragment listaMensajesFragment, listaFragmentCoches;
 
 
 
@@ -26,8 +27,10 @@ public class SecondActivity extends AppCompatActivity {
         DataHolder.instance.fireBaseAdmin.setListener(events);
 
         listaMensajesFragment=(ListaMensajesFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentListaMensajes);
+        listaFragmentCoches=(ListaMensajesFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentListaCoches);
 
         DataHolder.instance.fireBaseAdmin.descargarYObservarRama("messages");
+        DataHolder.instance.fireBaseAdmin.descargarYObservarRama("Coches");
 
 
 
@@ -60,14 +63,29 @@ class SecondActivityEvents implements FireBaseAdminListener{
 
         Log.v("SecondActivity", rama + "--------"+dataSnapshot);
 
+        if(rama.equals("messages")){
+            GenericTypeIndicator<Map<String,Mensaje>> indicator = new GenericTypeIndicator<Map<String,Mensaje>>(){};
+            Map<String,Mensaje> msgs=dataSnapshot.getValue(indicator);
+            Log.v("SecondActivity", "OBJETO MENSAJE TIENE: " + msgs);
+
+            ListaMensajesAdapter listaMensajesAdapter = new ListaMensajesAdapter(new ArrayList<Mensaje>(msgs.values()));
+            secondActivity.listaMensajesFragment.recyclerView.setAdapter(listaMensajesAdapter);
+
+        }else if(rama.equals("Coches")){
+            GenericTypeIndicator<Map<String,FBCoche>> indicator = new GenericTypeIndicator<Map<String,FBCoche>>(){};
+            Map<String,FBCoche> coches=dataSnapshot.getValue(indicator);
+            Log.v("SecondActivity", "coches MENSAJE TIENE: " + coches);
+
+            ListaCochesAdapter listaCochesAdapter = new ListaCochesAdapter(coches);
+            secondActivity.listaFragmentCoches.recyclerView.setAdapter(listaCochesAdapter);
+
+        }
+
+
+
         //Mensaje mensaje = dataSnapshot.getValue(Mensaje.class);
 
-        GenericTypeIndicator<Map<String,Mensaje>> indicator = new GenericTypeIndicator<Map<String,Mensaje>>(){};
-        Map<String,Mensaje> msgs=dataSnapshot.getValue(indicator);
-        Log.v("SecondActivity", "OBJETO MENSAJE TIENE: " + msgs);
 
-        ListaMensajesAdapter listaMensajesAdapter = new ListaMensajesAdapter(new ArrayList<Mensaje>(msgs.values()));
-        secondActivity.listaMensajesFragment.recyclerView.setAdapter(listaMensajesAdapter);
 
 
     }
