c1eaf3e67f3df3bf3fb894153155732bf48707d6
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-06-19 09:43:31 +0200

    Login
    Clases Login fragment y login fragment events y parte visual

--- app/src/main/java/com/example/alejandro/examenjuniopmdm/LoginFragment.java
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/LoginFragment.java
@@ -1,11 +1,74 @@
 package com.example.alejandro.examenjuniopmdm;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by alejandro on 19/6/18.
  */
 
-public class LoginFragment {
+public class LoginFragment extends Fragment {
+    public EditText etUsername;
+    public EditText etPassword;
+    public Button btnLogin;
+    public Button btnRegistrarse;
+
+    public LoginFragmentEvents events;
+    public LoginFragmentListener listener;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public void setListener(LoginFragmentListener listener){
+        this.listener=listener;
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_login, container, false);
+        etUsername=v.findViewById(R.id.etusername);
+        etPassword=v.findViewById(R.id.etpassword);
+        btnLogin=v.findViewById(R.id.btnlogin);
+        btnRegistrarse=v.findViewById(R.id.btnregistrarse);
+        events = new LoginFragmentEvents(this);
+        btnLogin.setOnClickListener(events);
+        btnRegistrarse.setOnClickListener(events);
+
+        return v;
+    }
 }
-class LoginFragmentEvents{
+class LoginFragmentEvents implements View.OnClickListener {
+
+    private LoginFragment loginFragment;
+
+    public LoginFragmentEvents(LoginFragment fragment){
+
+        this.loginFragment = fragment;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId()==this.loginFragment.btnLogin.getId()){
+            this.loginFragment.listener.loginFragmentLoginButtonClicked(
+                    this.loginFragment.etUsername.getText().toString(),
+                    this.loginFragment.etPassword.getText().toString());
+
+        }else if(view.getId()==this.loginFragment.btnRegistrarse.getId()) {
+            this.loginFragment.listener.loginFragmentRegisterButtonClicked();
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/LoginFragmentListener.java
@@ -0,0 +1,10 @@
+package com.example.alejandro.examenjuniopmdm;
+
+/**
+ * Created by alejandro on 19/6/18.
+ */
+
+public interface LoginFragmentListener {
+    public void loginFragmentLoginButtonClicked(String sUser, String sPassword);
+    public void loginFragmentRegisterButtonClicked();
+}
