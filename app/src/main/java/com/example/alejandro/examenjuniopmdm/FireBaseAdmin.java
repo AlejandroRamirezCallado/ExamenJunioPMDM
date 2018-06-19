81c15967846db210a159f4678049654fc416d386
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-06-19 10:39:09 +0200

    FireBase1
    Clases y configuracion gradle

---
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/FireBaseAdmin.java
@@ -0,0 +1,8 @@
+package com.example.alejandro.examenjuniopmdm;
+
+/**
+ * Created by alejandro on 19/6/18.
+ */
+
+public class FireBaseAdmin {
+}
---
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/FireBaseAdminListener.java
@@ -0,0 +1,8 @@
+package com.example.alejandro.examenjuniopmdm;
+
+/**
+ * Created by alejandro on 19/6/18.
+ */
+
+interface FireBaseAdminListener {
+}
