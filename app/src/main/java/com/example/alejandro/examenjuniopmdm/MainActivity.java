bce2434363b41b4fccec343a5c16223aaae89ac5
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-06-19 10:12:47 +0200

    Register
    RegisterFragment + events + listener + parte visual

--- app/src/main/java/com/example/alejandro/examenjuniopmdm/MainActivity.java
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.alejandro.examenjuniopmdm;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginFragment loginFragment;
+    RegisterFragment registerFragment;
+
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment=(RegisterFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentLogin);
+        registerFragment=(RegisterFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentRegister);
+        MainActivityEvents mainActivityEvents=new MainActivityEvents(this);
+
+        loginFragment.setListener(mainActivityEvents);
+        registerFragment.setListener(mainActivityEvents);
+
     }
 }
-class MainActivityEvents {
+class MainActivityEvents implements LoginFragmentListener, RegisterFragmentListener {
 
+    MainActivity mainActivity;
+    public MainActivityEvents(MainActivity mainActivity){
+
+        this.mainActivity=mainActivity;
+    }
+
+    @Override
+    public void loginFragmentLoginButtonClicked(String sUser, String sPassword) {
+
+    }
+
+    @Override
+    public void loginFragmentRegisterButtonClicked() {
+        FragmentTransaction transaction = mainActivity.getSupportFragmentManager().beginTransaction();
+        transaction.hide(mainActivity.loginFragment);
+        transaction.show(mainActivity.registerFragment);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void registerFragmentBtnAceptarClicked(String sUser, String sPass) {
+
+    }
+
+    @Override
+    public void registerFragmentBtnCancelarClicked() {
+
+    }
 }
---
+++ app/src/main/java/com/example/alejandro/examenjuniopmdm/RegisterFragmentListener.java
@@ -0,0 +1,11 @@
+package com.example.alejandro.examenjuniopmdm;
+
+/**
+ * Created by alejandro on 19/6/18.
+ */
+
+interface RegisterFragmentListener {
+
+    public void registerFragmentBtnAceptarClicked(String sUser, String sPass);
+    public void registerFragmentBtnCancelarClicked();
+}
